ccffd40684890c11f12027b48658fca5be9f7914
Red <crabmarket56@gmail.com>
2022-07-12 11:42:03 -0400

    Update comments

--- script.rb
+++ script.rb
@@ -1,6 +1,6 @@
 # takes input string and integer, splits each string into an array of individual characters,
 # unless the target character is a space, '?', or '!', compares each character in the array to an array of the alphabet, 
-# adds the shift integer to the alphabet arrays index integer and returns the letter at the resulting index in the alphabet array
+# adds the shift integer to the alphabet arrays index integer and returns the case sensitive letter at the resulting index in the alphabet array
 # if the result of adding the two integers is greater than or equal to 26, the resulting index loops back to 0
 require 'pry-byebug'
 
